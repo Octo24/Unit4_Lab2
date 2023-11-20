a4d5c758fa3555d5e2b13f9c637ee4d9ac3cc618
Octo24 <octavian.hagi-memet@edu.sait.ca>
2023-11-19 20:02:07 -0700

    added doc strings

--- module.py
+++ module.py
@@ -1,14 +1,18 @@
 class BankAccount:
+    '''Bank Account information and funds'''
     def __init__(self):
+        '''initialize name, number and balance'''
         self.accountNumber = input("Enter Account Number: ")
         self.customerName = input("Enter Customer Name: ")
         self.balance = float(input("Enter Initial Balance: "))  # Assuming balance is a float
 
     def deposit(self, amount):
+        '''deposit to bank account'''
         self.balance += amount
         print(f"Deposited ${amount}. New balance: ${self.balance}")
 
     def withdraw(self, amount):
+        '''withdraw from bank account'''
         if amount > self.balance:
             print("Insufficient funds. Withdrawal not allowed.")
         else:
@@ -16,6 +20,7 @@ class BankAccount:
             print(f"Withdrew ${amount}. New balance: ${self.balance}")
 
     def display(self):
+        '''display customer information'''
         print(f"Account Number: {self.accountNumber}")
         print(f"Customer Name: {self.customerName}")
         print(f"Balance: ${self.balance}")
